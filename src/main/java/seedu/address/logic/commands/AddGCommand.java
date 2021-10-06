447ba7297430f466dd205491bf7c506f941810c7
Asher <77272176+asherhy@users.noreply.github.com>
2021-10-07 06:13:56 +0800

    Add AddGCommand and AddGCommandParser

---
+++ src/main/java/seedu/address/logic/commands/AddGCommand.java
@@ -0,0 +1,43 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.group.Group;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
+public class AddGCommand extends Command {
+    public static final String COMMAND_WORD = "addG";
+
+    public static final String MESSAGE_SUCCESS = "New group added: %1$s";
+    public static final String MESSAGE_DUPLICATE_GROUP = "This group already exists in the address book";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a group to the address book. "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + "\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "Favourite Group";
+
+    private final Group toAdd;
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasGroup(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_GROUP);
+        }
+
+        model.addGroup(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AddCommand // instanceof handles nulls
+                && toAdd.equals(((AddCommand) other).toAdd));
+    }
+}
