47db9ab20172d94e77b6cc50b19099b333f2f6fc
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-10-16 17:18:47 +0800

    Add test cases for Id::equals

--- src/test/java/seedu/address/storage/IdTest.java
+++ src/test/java/seedu/address/storage/IdTest.java
@@ -1,6 +1,7 @@
 package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -75,5 +76,31 @@ public class IdTest {
     public void parse_invalidTimeRange_throwsIllegalValueException() {
         String expectedMessage = Id.MESSAGE_MALFORMED_ID;
         assertThrows(IllegalValueException.class, expectedMessage, () -> Id.parse("1-4e94914f0000"));
+    }
+
+    @Test
+    public void equals_differentType_returnsFalse() {
+        assertNotEquals(null, Id.generateId());
+        assertNotEquals(new IdTest(), Id.generateId());
+    }
+
+    @Test
+    public void equals_sameReference_returnsTrue() {
+        Id id = Id.generateId();
+        assertEquals(id, id);
+    }
+
+    @Test
+    public void equals_differentDateTime_returnsFalse() throws IllegalValueException {
+        Id id1 = Id.parse("0-1");
+        Id id2 = Id.parse("1-2");
+        assertNotEquals(id1, id2);
+    }
+
+    @Test
+    public void equals_sameDateTime_returnsTrue() throws IllegalValueException {
+        Id id1 = Id.parse("1-2");
+        Id id2 = Id.parse("1-2");
+        assertEquals(id1, id2);
     }
 }
