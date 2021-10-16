458cffdf0bc35ab6a1613563db213bdb02c03018
Nikki <thanwinnikki@gmail.com>
2021-10-16 17:34:12 +0800

    Add GroupBuilder class.

---
+++ src/test/java/seedu/address/model/group/GroupTest.java
@@ -0,0 +1,2 @@
+package seedu.address.model.group;public class GroupTest {
+}
---
+++ src/test/java/seedu/address/testutil/GroupBuilder.java
@@ -0,0 +1,58 @@
+package seedu.address.testutil;
+
+import seedu.address.model.group.Group;
+import seedu.address.model.names.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.UniquePersonList;
+
+/**
+ * A utility class to help with building Group objects.
+ */
+public class GroupBuilder {
+
+    public static final String DEFAULT_GROUP_NAME = "CS2103T";
+
+    private Name name;
+    private UniquePersonList persons;
+
+    /**
+     * Creates a {@code GroupBuilder} with the default details.
+     */
+    public GroupBuilder() {
+        name = new Name(DEFAULT_GROUP_NAME);
+        persons = new UniquePersonList();
+    }
+
+    /**
+     * Initializes the GroupBuilder with the data of {@code groupToCopy}.
+     */
+    public GroupBuilder(Group groupToCopy) {
+        name = groupToCopy.getName();
+        persons = new UniquePersonList();
+        persons.setPersons(groupToCopy.getPersons());
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Group} that we are building.
+     */
+    public GroupBuilder withName(String name) {
+        this.name = new Name(name);
+        return this;
+    }
+
+    /**
+     * Sets the {@code UniquePersonList} of the {@code Group} to contain specified {@code Person}.
+     * @return
+     */
+    public GroupBuilder withMembers(Person... persons) {
+        for (Person person : persons) {
+            this.persons.add(person);
+        }
+        return this;
+    }
+
+    public Group build() {
+        return new Group(name);
+    }
+
+}
