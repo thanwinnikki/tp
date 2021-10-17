a0ac51234a83d8dd745eaddeb7c33b40e35abf5c
Nguyen Thuan Hung <e0550385@u.nus.edu>
2021-10-17 21:59:56 +0800

    Resolved merge conflict

--- src/test/java/seedu/address/logic/parser/DeleteGroupCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteGroupCommandParserTest.java
@@ -3,7 +3,7 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_GROUP;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,7 +22,7 @@ public class DeleteGroupCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
-        assertParseSuccess(parser, "1", new DeleteGroupCommand(INDEX_FIRST_GROUP));
+        assertParseSuccess(parser, "1", new DeleteGroupCommand(INDEX_FIRST));
     }
 
     @Test
