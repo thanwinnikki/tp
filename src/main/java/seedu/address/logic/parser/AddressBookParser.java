e1a33fad96299b0180076932eb37ef5716c6af02
Nikki <thanwinnikki@gmail.com>
2021-10-22 01:00:25 +0800

    Add DeleteTCommand related classes.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -12,6 +12,7 @@ import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.DeleteGroupCommand;
+import seedu.address.logic.commands.DeleteTCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
@@ -93,6 +94,9 @@ public class AddressBookParser {
 
         case AddGCommand.COMMAND_WORD:
             return new AddGCommandParser().parse(arguments);
+
+        case DeleteTCommand.COMMAND_WORD:
+            return new DeleteTCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
