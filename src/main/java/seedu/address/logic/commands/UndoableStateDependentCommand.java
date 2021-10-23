e642b4475b7b75be14b27c36b2edd7bfbf5a213d
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-10-23 19:53:12 +0800

    Add UndoableStateDependentCommand

---
+++ src/main/java/seedu/address/logic/commands/UndoableStateDependentCommand.java
@@ -0,0 +1,19 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Represents a {@code StateDependentCommand} that can be undone.
+ */
+public interface UndoableStateDependentCommand extends StateDependentCommand {
+
+    /**
+     * Undoes the command and returns the result message.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return The result of undoing the command including the feedback message of the operation result for display.
+     * @throws CommandException If an error occurs during command execution.
+     */
+    CommandResult undo(Model model) throws CommandException;
+}
