4b6c84bba06ccf68ca608ca1201927dd9326fa91
Asher <77272176+asherhy@users.noreply.github.com>
2021-10-23 16:12:38 +0800

    Update EditGroupCommandParser.java

--- src/main/java/seedu/address/logic/parser/EditGroupCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditGroupCommandParser.java
@@ -2,24 +2,14 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
-import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.EditGroupCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.tag.Tag;
 
-public class EditGroupCommandParser implements Parser<EditGroupCommand>{
+public class EditGroupCommandParser implements Parser<EditGroupCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the EditGroupCommand
@@ -44,7 +34,8 @@ public class EditGroupCommandParser implements Parser<EditGroupCommand>{
             editGroupDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
         }
         if (argMultimap.getValue(PREFIX_DESCRIPTION).isPresent()) {
-            editGroupDescriptor.setDescription(ParserUtil.parseDescription(argMultimap.getValue(PREFIX_DESCRIPTION).get()));
+            editGroupDescriptor.setDescription(ParserUtil
+                    .parseDescription(argMultimap.getValue(PREFIX_DESCRIPTION).get()));
         }
         if (!editGroupDescriptor.isAnyFieldEdited()) {
             throw new ParseException(EditGroupCommand.MESSAGE_NOT_EDITED);
