9ab8e3c5042b8b190819317792bb8f4398167992
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-10-23 21:36:21 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/AlwaysRunnableCommand.java
+++ src/main/java/seedu/address/logic/commands/AlwaysRunnableCommand.java
@@ -1,8 +1,6 @@
 package seedu.address.logic.commands;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.state.ApplicationState;
-import seedu.address.model.Model;
 
 /**
  * Represents a command with hidden internal logic and the ability to be executed.
