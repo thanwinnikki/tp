b33066ade9edfe1a686d3b7ae561a408fa29a25d
Asher <77272176+asherhy@users.noreply.github.com>
2021-10-27 23:49:20 +0800

    Update AddressBook.java

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -172,7 +172,6 @@ public class AddressBook implements ReadOnlyAddressBook {
 
     @Override
     public boolean equals(Object other) {
-        boolean test2 = groups.equals(((AddressBook) other).groups);
         return other == this // short circuit if same object
                 || (other instanceof AddressBook // instanceof handles nulls
                 && persons.equals(((AddressBook) other).persons)
