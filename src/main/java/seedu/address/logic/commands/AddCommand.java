41dd5c2f7c2ffba64d199df020dbb639fbca3782
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-10-29 01:39:25 +0800

    Update add command feedback message

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -23,7 +23,7 @@ public class AddCommand extends AlwaysRunnableCommand implements UndoableCommand
             + PREFIX_NAME + "NAME "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
-            + PREFIX_ADDRESS + "ADDRESS "
+            + "[" + PREFIX_ADDRESS + "ADDRESS] "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
