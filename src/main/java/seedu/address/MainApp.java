199cb3ebf31d7a7aaf341126ed394971cca82280
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-10-29 10:10:53 +0800

    Revert release v1.3.3

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -36,7 +36,7 @@ import seedu.address.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 3, 3, false);
+    public static final Version VERSION = new Version(1, 3, 2, false);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
