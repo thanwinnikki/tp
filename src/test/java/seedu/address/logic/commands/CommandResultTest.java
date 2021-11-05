4deb9e4c446adfe7694a7f0ee8d280f3f9e2fc3e
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-11-05 15:15:09 +0800

    Add tests for CommandResult.Builder

--- src/test/java/seedu/address/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/address/logic/commands/CommandResultTest.java
@@ -8,7 +8,109 @@ import static seedu.address.testutil.TypicalGroups.CS2103T_GROUP_BUILDER;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.state.ApplicationState;
+import seedu.address.logic.state.GroupInformationState;
+import seedu.address.logic.state.HomeState;
+import seedu.address.model.group.Group;
+
 public class CommandResultTest {
+
+    @Test
+    public void build_onlyFeedbackSpecified_defaultValuesReturned() {
+        // Equivalence Partition {isGoingToShowHelp, isGoingToExit, nextApplicationState, isGoingToCauseUndo}: None
+        // specified
+        CommandResult commandResult = new CommandResult.Builder("feedback")
+                .build();
+
+        assertFalse(commandResult.isGoingToShowHelp());
+        assertFalse(commandResult.isGoingToExit());
+        assertEquals(new HomeState(), commandResult.getNextApplicationState());
+        assertFalse(commandResult.isGoingToCauseUndo());
+    }
+
+    @Test
+    public void build_setShowHelp_success() {
+        // Equivalence Partition {isGoingToShowHelp}: Specified false
+        CommandResult commandResult = new CommandResult.Builder("feedback")
+                .setGoShowHelp(false)
+                .build();
+        assertFalse(commandResult.isGoingToShowHelp());
+
+        // Equivalence Partition {isGoingToShowHelp}: Specified true
+        commandResult = new CommandResult.Builder("feedback")
+                .setGoShowHelp(true)
+                .build();
+        assertTrue(commandResult.isGoingToShowHelp());
+        commandResult = new CommandResult.Builder("feedback")
+                .goShowHelp()
+                .build();
+        assertTrue(commandResult.isGoingToShowHelp());
+    }
+
+    @Test
+    public void build_setGoExit_success() {
+        // Equivalence Partition {isGoingToExit}: Specified false
+        CommandResult commandResult = new CommandResult.Builder("feedback")
+                .setGoExit(false)
+                .build();
+        assertFalse(commandResult.isGoingToExit());
+
+        // Equivalence Partition {isGoingToExit}: Specified true
+        commandResult = new CommandResult.Builder("feedback")
+                .setGoExit(true)
+                .build();
+        assertTrue(commandResult.isGoingToExit());
+        commandResult = new CommandResult.Builder("feedback")
+                .goExit()
+                .build();
+        assertTrue(commandResult.isGoingToExit());
+    }
+
+    @Test
+    public void build_setNextApplicationState_success() {
+        ApplicationState nextApplicationState = new HomeState();
+        // Equivalence Partition {nextApplicationState}: Specified as HomeState
+        CommandResult commandResult = new CommandResult.Builder("feedback")
+                .setNextApplicationState(nextApplicationState)
+                .build();
+        assertEquals(nextApplicationState, commandResult.getNextApplicationState());
+        commandResult = new CommandResult.Builder("feedback")
+                .goToHome()
+                .build();
+        assertEquals(nextApplicationState, commandResult.getNextApplicationState());
+
+        // Equivalence Partition {nextApplicationState}: Specified as GroupInformationState
+        Group group = CS2103T_GROUP_BUILDER.build();
+        nextApplicationState = new GroupInformationState(group);
+        commandResult = new CommandResult.Builder("feedback")
+                .setNextApplicationState(nextApplicationState)
+                .build();
+        assertEquals(nextApplicationState, commandResult.getNextApplicationState());
+        commandResult = new CommandResult.Builder("feedback")
+                .displayGroupInformation(group)
+                .build();
+        assertEquals(nextApplicationState, commandResult.getNextApplicationState());
+    }
+
+    @Test
+    public void build_setGoCauseUndo_success() {
+        // Equivalence Partition {isGoingToCauseUndo}: Specified false
+        CommandResult commandResult = new CommandResult.Builder("feedback")
+                .setGoCauseUndo(false)
+                .build();
+        assertFalse(commandResult.isGoingToCauseUndo());
+
+        // Equivalence Partition {isGoingToCauseUndo}: Specified true
+        commandResult = new CommandResult.Builder("feedback")
+                .setGoCauseUndo(true)
+                .build();
+        assertTrue(commandResult.isGoingToCauseUndo());
+        commandResult = new CommandResult.Builder("feedback")
+                .goCauseUndo()
+                .build();
+        assertTrue(commandResult.isGoingToCauseUndo());
+    }
+
     @Test
     public void equals() {
         CommandResult commandResult = new CommandResult("feedback");
@@ -38,7 +140,7 @@ public class CommandResultTest {
         assertFalse(commandResult.equals(new CommandResult("feedback", true, false)));
         assertFalse(commandResult.equals(
                 new CommandResult.Builder("feedback")
-                        .showHelp()
+                        .goShowHelp()
                         .build()
         ));
 
@@ -80,7 +182,7 @@ public class CommandResultTest {
         // different isGoingToShowHelp value -> returns different hashcode
         assertNotEquals(commandResult.hashCode(), new CommandResult("feedback", true, false).hashCode());
         assertNotEquals(commandResult.hashCode(), new CommandResult.Builder("feedback")
-                .showHelp()
+                .goShowHelp()
                 .build().hashCode());
 
         // different isGoingToExit value -> returns different hashcode
